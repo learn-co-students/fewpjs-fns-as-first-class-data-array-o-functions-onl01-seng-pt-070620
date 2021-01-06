cb8052fe469d3e7275eeda2017624b0eadb065b8
katiethile <lethikatie@gmail.com>
2021-01-06 00:51:24 -0800

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-let wakeDog = function(dogName="Byron", dogBreed="poodle") {
-    console.log(`Wake ${dogName} the ${dogBreed}`);
-    return `Wake ${dogName} the ${dogBreed}`
+    function wakeDog(dogName="Byron", dogBreed="poodle") {
+    let string = (`Wake ${dogName} the ${dogBreed}`);
+    console.log(string)
+    return string 
 }
 
-let leashDog = function(dogName="Byron", dogBreed="poodle") {
-    console.log(`Leash ${dogName} the ${dogBreed}`)
-    return `Leash ${dogName} the ${dogBreed}`
+    function leashDog(dogName="Byron", dogBreed="poodle") {
+    let string = (`Leash ${dogName} the ${dogBreed}`)
+    console.log(string)
+    return string 
 }
 
-let walkToPark = function(dogName="Byron", dogBreed="poodle") {
-    console.log(`Walk to the park with ${dogName} the ${dogBreed}`)
-    return `Walk to the park with ${dogName} the ${dogBreed}`
+    function walkToPark(dogName="Byron", dogBreed="poodle") {
+    let string = (`Walk to the park with ${dogName} the ${dogBreed}`)
+    console.log(string)
+    return string 
 }
 
-let throwFrisbee = function(dogName="Byron", dogBreed="poodle") {
-    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`)
-    return `Throw the frisbee for ${dogName} the ${dogBreed}`
+    function throwFrisbee(dogName="Byron", dogBreed="poodle") {
+    let string = (`Throw the frisbee for ${dogName} the ${dogBreed}`)
+    console.log(string)
+    return string 
 }
 
-let walkHome = function(dogName="Byron", dogBreed="poodle") {
-    console.log(`Walk home with ${dogName} the ${dogBreed}`)
-    return `Walk home with ${dogName} the ${dogBreed}`
+    function walkHome(dogName="Byron", dogBreed="poodle") {
+    let string = (`Walk home with ${dogName} the ${dogBreed}`)
+    console.log(string)
+    return string 
 }
-let unleashDog = function(dogName="Byron", dogBreed="poodle") {
-    console.log(`Unleash ${dogName} the ${dogBreed}`)
-    return `Unleash ${dogName} the ${dogBreed}`
+    function unleashDog(dogName="Byron", dogBreed="poodle") {
+    let string = (`Unleash ${dogName} the ${dogBreed}`)
+    console.log(string)
+    return string 
 }
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
-function exerciseDog (name, breed) {
-    return routine.map(fn => fn (name, breed))
+function exerciseDog (dogName, dogBreed) {
+    return routine.map(fn => fn (dogName, dogBreed))
 }
